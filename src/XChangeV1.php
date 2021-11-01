b1ae4cb068c46b9304edfc75e48a767881aecd50
jimmyjetter27 <jimmyjetter27@gmail.com>
2021-11-01 11:59:40 +0000

    live url

--- src/XChangeV1.php
+++ src/XChangeV1.php
@@ -25,9 +25,9 @@ class XChangeV1 extends API
     protected $secret_key;
     protected $client_key;
     protected $client_id;
-//    protected static $live_url = 'https://xchange.korba365.com/api/v1.0';
+    protected static $live_url = 'https://xchange.korba365.com/api/v1.0';
 //    protected static $live_url = 'https://xchange.korbaweb.com/api/v1.0'; // Cyber Sai's url
-    protected static $live_url = 'https://khc1qo46gk-vpce-0966664bebf81f8fa.execute-api.eu-west-1.amazonaws.com/private '; // new private (smb) exchange url
+//    protected static $live_url = 'https://khc1qo46gk-vpce-0966664bebf81f8fa.execute-api.eu-west-1.amazonaws.com/private '; // new private (smb) exchange url
 //    protected static $test_url = 'https://korba-xchange.herokuapp.com/api/v1.0';
     protected static $test_url = 'https://xchange.korba365.com/api/v1.0/'; // Rais Test
     protected static $aws_url = 'http://internal-awseb-e-e-awsebloa-kxexw3t2bgt7-1521297916.eu-west-1.elb.amazonaws.com/api/v1.0';
