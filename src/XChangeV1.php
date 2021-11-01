489676628d74c0160ea00c66ac061427a3130d4d
jimmyjetter27 <jimmyjetter27@gmail.com>
2021-11-01 16:14:01 +0000

    mtn bundle method

--- src/XChangeV1.php
+++ src/XChangeV1.php
@@ -489,9 +489,7 @@ class XChangeV1 extends API
             foreach ($result as $firstBundles) {
                 foreach ($firstBundles['bundles'] as $secondBundles) {
                     Log::debug($secondBundles);
-                    foreach ($secondBundles['bundles'] as &$bundle) {
-//                        echo $bundle['short_name'];
-                        return gettype($bundle);
+                    return $secondBundles;
                     }
                 }
             }
