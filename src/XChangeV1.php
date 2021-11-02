cc70aca5e0e1cd3cb0e1dd77ba11fd8bd53fbcee
jimmyjetter27 <jimmyjetter27@gmail.com>
2021-11-02 14:35:15 +0000

    readed Sai's live url

--- src/XChangeV1.php
+++ src/XChangeV1.php
@@ -25,6 +25,8 @@ class XChangeV1 extends API
     protected $secret_key;
     protected $client_key;
     protected $client_id;
+
+//    protected static $live_url = 'https://xchange.korbaweb.com/api/v1.0';
     protected static $aws_url = 'http://internal-awseb-e-e-awsebloa-kxexw3t2bgt7-1521297916.eu-west-1.elb.amazonaws.com/api/v1.0';
 
     public function __construct($secret_key, $client_key, $client_id, $mode = 'test', $proxy = null)
